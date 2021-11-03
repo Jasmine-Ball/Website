9ebc25f859fa3daee17e2eacb0fa07367c243a2c
Jasmine-Ball <jasmine.ball@uon.edu.au>
2021-11-03 21:12:37 +1100

    Add App.jsx including fetch

---
+++ src/articles/src/App.jsx
@@ -0,0 +1,25 @@
+// Jasmine Ball 3/11/2021
+import React, {useState, useEffect} from 'react';
+
+const api = 'https://www.jasmineball.net/philosophy-api/';
+
+const FetchAPI = () => {
+    const [body, setBody] = useState([]);
+    const getBody = async () => {
+        const re = await fetch(api);
+        const body = await re.json();
+        setBody(body);
+    };
+
+    useEffect( () => {
+        getBody();
+    }, []);
+
+    return (
+        <>
+        {body.id}
+        </>
+    );
+};
+
+export default FetchAPI;
